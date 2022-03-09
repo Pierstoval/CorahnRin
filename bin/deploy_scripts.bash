7b1dca81a5de35a505e0b4fa81a4c3e1a2eb64a2
Alex Rock Ancelet <pierstoval@gmail.com>
2022-03-09 15:51:18 +0100

    Fix deploy issue for map data

--- bin/deploy_scripts.bash
+++ bin/deploy_scripts.bash
@@ -19,9 +19,6 @@ $HOME/bin/composer install --no-dev --no-scripts --prefer-dist --optimize-autolo
 php bin/console cache:clear --no-warmup
 php bin/console cache:warmup
 
-php _dev_files/get_maps_assets.php
-php _dev_files/get_maps_data.php
-
 php bin/console doctrine:migrations:migrate --no-interaction
 
 php bin/console doctrine:schema:validate || echo 'Doctrine schema not valid, please make sure it is correct.'
@@ -29,3 +26,7 @@ php bin/console doctrine:schema:validate || echo 'Doctrine schema not valid, ple
 yarn install
 
 yarn run build
+
+# Execute these AFTER yarn build, because it modifies the generated JSON manifest.
+php _dev_files/get_maps_assets.php
+php _dev_files/get_maps_data.php
