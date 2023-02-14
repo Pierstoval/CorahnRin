1da27663c47f54ad2643bc424ad3f414b3c551a1
Alex Rock Ancelet <pierstoval@gmail.com>
2023-02-14 23:05:09 +0100

    Refactor http client login in tests

--- tests/GetHttpClientTestTrait.php
+++ tests/GetHttpClientTestTrait.php
@@ -15,10 +15,7 @@ namespace Tests;
 
 use Symfony\Bundle\FrameworkBundle\KernelBrowser;
 use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
-use Symfony\Component\BrowserKit\Cookie;
-use Symfony\Component\Security\Core\Authentication\AuthenticationManagerInterface;
-use Symfony\Component\Security\Core\User\UserInterface;
-use Symfony\Component\Security\Guard\Token\PostAuthenticationGuardToken;
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 use User\Repository\UserRepository;
 
 trait GetHttpClientTestTrait
@@ -38,42 +35,21 @@ trait GetHttpClientTestTrait
             self::fail(\sprintf('Cannot find user "%s" to log in.', $username));
         }
 
-        self::setToken($client, $user, $user->getRoles());
+        $client->loginUser($user);
     }
 
     protected function getHttpClient(): KernelBrowser
     {
-        if (!$this instanceof KernelTestCase) {
-            throw new \RuntimeException(\sprintf('Test case must extend %s to use Kernel features', KernelTestCase::class));
+        if (!$this instanceof WebTestCase) {
+            throw new \RuntimeException(\sprintf('Test case must extend %s to use Kernel features', WebTestCase::class));
         }
 
         $server = [];
 
-        /** @var KernelBrowser $client */
         $client = self::createClient([], $server);
         // Disable reboot, allows client to be reused for other requests.
         $client->disableReboot();
 
         return $client;
-    }
-
-    protected static function setToken(KernelBrowser $client, UserInterface $user, array $roles = ['ROLE_USER']): void
-    {
-        if (!\is_a(self::class, KernelTestCase::class, true)) {
-            throw new \RuntimeException(\sprintf('Test case must extend %s to use Kernel features', KernelTestCase::class));
-        }
-
-        $firewallName = 'main';
-
-        $session = $client->getContainer()->get('session');
-
-        $token = new PostAuthenticationGuardToken($user, $firewallName, $roles);
-        self::getContainer()->get(AuthenticationManagerInterface::class)->authenticate($token);
-
-        $session->set('_security_'.$firewallName, \serialize($token));
-        $session->save();
-
-        $cookie = new Cookie($session->getName(), $session->getId());
-        $client->getCookieJar()->set($cookie);
     }
 }
